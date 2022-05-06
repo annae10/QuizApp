8f4586df464aef136334f0fc436b493c57b6f669
Anna Ermolaeva <ermolaeva-ann@mail.ru>
2022-05-06 23:51:32 +0300

    Question screen

--- app/src/main/java/com/harman/quizapp/Category.kt
+++ app/src/main/java/com/harman/quizapp/Category.kt
@@ -1,11 +1,20 @@
 package com.harman.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Category : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        val start_button: ImageButton = findViewById(R.id.startBtn)
+        val intent: Intent = Intent(this, Questions::class.java)
+
+        start_button.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
